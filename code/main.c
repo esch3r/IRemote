6e652bfd82846f2cbe3f010b3a09a5ed8d1d5531
Alexander Rössler <mail.aroessler@gmail.com>
2012-12-05 12:32:48 +0100

    something

--- code/main.c
+++ code/main.c
@@ -21,27 +21,10 @@ int main(void)
     initializeUart0(115200);                    // Init the UART
     printfUart0("Welcome to my Program!\n");    // Send a welcome message
     
-    initializeDac();
-    initializeAdc(100000,2,0);
-    //setDacVolt(2200);
-
-    uint16 f = 1;
-    double omega = 2* M_PI*f;
-    uint32 t = 0;
-    uint32 time = 0;
-    
-    uint16 adcValue = 50;
-    
     for (;;) 
     {
         delayMs(10);
-        t++;
-        time++;
-        if (t > 100)
-            t=1;
-        setDacValue((uint16)(511.5*sin(omega*((double)t/100.0))+511.5));
-        readAdc(&adcValue);
-        printfUart0("%i %i\n", time, adcValue);
+
      }
 
     return 0 ;
