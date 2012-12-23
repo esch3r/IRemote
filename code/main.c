ffc81f363490e1f04d018f375da407157d97bc01
Alexander Rössler <mail.aroessler@gmail.com>
2012-12-23 18:13:28 +0100

    added a lot og stuff

--- code/main.c
+++ code/main.c
@@ -4,26 +4,28 @@
 
 #include <led.h>
 #include <iap.h>
+#include <wifly.h>
 #include "irControl.h"
 
 typedef enum {
     ApplicationStateIdle = 0,
     ApplicationStateCaptureCommand = 1,
-    ApplicationStateRunCommand = 2
+    ApplicationStateRunCommand = 2,
+    ApplicationStateWiFlyTest = 100
 } ApplicationState;
 
 void testFunc();
 void startState(ApplicationState state);
+void processCommand(void);
 
 uint8 testMode = 0;
 IrCommand *testCommand;
 uint8 applicationState = ApplicationStateIdle;
+char commandBuffer[100];
+uint8 commandBufferPos = 0;
 
 int main(void)
 {   
-    uint32 testVar;
-    char   testChar;
-    
     initializeLeds();
     clearAllLeds();
     
@@ -33,18 +35,25 @@ int main(void)
     delayMs(500);
     
     initializeUart0(115200);                    // Init the UART
+    char testChar;
+     while (getcharUart0(&testChar) == 0)
+            ;
     printfUart0("Welcome to IRemote!\n");    // Send a welcome message
     printfUart0("Id: %i, Version: %i, Serial: %i\n",readIdIap(),readVersionIap(),readSerialIap());
+   
+    // Testing IAP functions
+    //    uint32 testVar;
     //printfUart0("Sector 26: %i, Sector 27: %i\n",checkBlankIap(26),checkBlankIap(27));
-    testVar = 0;
-    __disable_irq();
+    //testVar = 0;
+    //__disable_irq();
     //eraseIap(27);
     //writeIap(27,256*64,(void*)(&testVar),sizeof(testVar));
-    readIap(27,256*64,(void*)(&testVar),sizeof(testVar));
-    printfUart0("Test: %u\n",testVar);
-    __enable_irq();
+    //readIap(27,256*64,(void*)(&testVar),sizeof(testVar));
+    //printfUart0("Test: %u\n",testVar);
+    //__enable_irq();
     
     initializeIrControl();
+    initializeWiFly();
     
     setPinMode(2,10,PinModeNoPullUpDown);       // button3
     setGpioDirection(2,10,GpioDirectionInput);
@@ -56,12 +65,30 @@ int main(void)
     {
         if (applicationState == ApplicationStateIdle)
         {
-            while (getcharUart0(&testChar) == 0)
+            static char receivedData;
+            while (getcharUart0(&receivedData) == 0)
             {
-                if (testChar == 'r')
-                    startState(ApplicationStateRunCommand);
-                else if (testChar == 'c')
-                    startState(ApplicationStateCaptureCommand);
+                if (receivedData != 13) // Carriage Return (CR)
+                {
+                    commandBuffer[commandBufferPos] = receivedData;
+                    commandBufferPos++;
+                }
+                else
+                {
+                    commandBuffer[commandBufferPos] = '\0';
+                    processCommand();
+                    if ((strlen("run") == commandBufferPos) 
+                        && (strncmp("run",commandBuffer,commandBufferPos) == 0))
+                        startState(ApplicationStateRunCommand);
+                    else if ((strlen("capture") == commandBufferPos) 
+                        && (strncmp("capture",commandBuffer,commandBufferPos) == 0))
+                        startState(ApplicationStateCaptureCommand);
+                    else if ((strlen("test wifly") == commandBufferPos) 
+                        && (strncmp("test wifly",commandBuffer,commandBufferPos) == 0))
+                        startState(ApplicationStateWiFlyTest);
+                    
+                    commandBufferPos = 0;
+                }
             }
         }
         else if (applicationState == ApplicationStateCaptureCommand)
@@ -77,6 +104,14 @@ int main(void)
         {
             if (!isCommandRunning())    // Command has finished
                 startState(ApplicationStateIdle);
+        }
+        else if (applicationState == ApplicationStateWiFlyTest)
+        {
+            static char receivedData;
+            while (getcharUart0(&receivedData) == 0)
+                putcharUart1(receivedData);
+            while (getcharUart1(&receivedData) == 0)
+                putcharUart0(receivedData);
         }
         delayMs(100);
      }
@@ -117,7 +152,7 @@ void startState(ApplicationState state)
         blinkLed2(1);
         startIrCapture();
     }
-    else if (state = ApplicationStateRunCommand)
+    else if (state == ApplicationStateRunCommand)
     {
         applicationState = ApplicationStateRunCommand;
                 
@@ -125,6 +160,84 @@ void startState(ApplicationState state)
         blinkLed(1);
         runIrCommand(testCommand);
     }
+    else if (state == ApplicationStateWiFlyTest)
+    {
+        applicationState = ApplicationStateWiFlyTest;
+        
+        printfUart0("Going into WiFly Test state, all uart in and outputs will redirected.\n");
+    }
     
     return;
+}
+
+bool compareBaseCommand(char *original, char *received)
+{
+    return (strcmp(original,received) == 0);
+}
+
+bool compareExtendedCommand(char *original, char *received)
+{
+    return (((strlen(received) == 1) && strncmp(original,received,1)) ||
+                strcmp(original,received) == 0);
+}
+
+void printUnknownCommand(void)
+{
+    printfUart0("CMD?\n");
+}
+
+void printParameterMissing(void)
+{
+    printfUart0("Missing parameter.\n");
+}
+
+void processCommand(void )
+{
+    char *dataPointer;
+    
+    dataPointer = strtok(commandBuffer," ");
+    
+    if (compareBaseCommand("run", dataPointer))
+    {
+        // We have a run command
+    }
+    else if (compareBaseCommand("capture", dataPointer))
+    {
+        // We have a capture command
+    }
+    else if (compareBaseCommand("set", dataPointer))
+    {
+        // starting a set command
+        //dataPointer = strtok(NULL," ");
+        if (strtok_r(NULL, " ", &dataPointer) == NULL)
+        {
+            printUnknownCommand();
+            return;
+        }
+        else if (compareExtendedCommand("wlan",dataPointer))
+        {
+            // set wlan
+            if (strtok_r(NULL, " ", &dataPointer) == NULL)
+            {
+                printUnknownCommand();
+            }
+            else if (compareExtendedCommand("ssid",dataPointer))
+            {
+                // set ssid
+            }
+            else
+            {
+                printUnknownCommand();
+                return;
+            }
+        }
+        else
+        {
+            printUnknownCommand();
+        }
+    }
+    else
+    {
+        printUnknownCommand();
+    }
 }
