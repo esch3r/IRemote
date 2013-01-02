7b396be86d2a099a06137ddc84a6d220c1cebce4
wimma <johannes.wimmer@gmx.at>
2013-01-02 14:50:25 +0000

    Button files splitted

--- code/Libraries/Library/inc/pwm.h
+++ code/Libraries/Library/inc/pwm.h
@@ -8,45 +8,25 @@
 #ifndef _PWM_DRIVER_H_
 #define _PWM_DRIVER_H_
 
-//#include "LPC17xx.h"
 #include <types.h>
 #include <LPC17xx.h>
 
 #define PCONP_PWM     (1 << 6) 		//set pwm1/clock control bit
 #define PWM_PDN        (1 << 21)     // 1 ADC on, 0 ADC off
 #define PCLK_PWM       (1 << 24)     // 0b00 CCLK/4, 0b01 CCLK, 0b10 CCLK/2, 0b11 CCLK/8
-
+#define PWM_SET_CORE_CLK()  LPC_SC->PCLKSEL0 |= (0b00<< 12); //set 12:13 for PWM1
 #define SYSTEM_CLK      SystemCoreClock
 #define PWM_CLK         SYSTEM_CLK/4
 
-
 #define PWM_ENABLE_POWER()  LPC_SC->PCONP |= PCONP_PWM
 #define PWM_DISABLE_POWER() LPC_SC->PCONP &= ~PCONP_PWM
-#define PWM_SET_CORE_CLK()  LPC_SC->PCLKSEL0 |= (0b00<< 12); //set 12:13 for PWM1 
 
-#define PWM_CLEAR_PINSEL()  LPC_ADC->ADCR &= ~0xFF
-
-#define PWM_ENABLE_PIN1()   LPC_PINCON->PINSEL3  |= (0b10 << 4); LPC_PINCON->PINMODE1 |= (0b00 << 4)  // PWM 1.1 func 10
-
-
-#define ADC_SET_CLK(x)      LPC_ADC->ADCR |= (((uint16)(ADC_CLK/x) - 1) << 8);
-#define ADC_CONFIGURE()     LPC_ADC->ADCR |= (0 << 24); LPC_ADC->ADCR |= (0 << 27); // No start and CAP/MAT rising edge trigger
-#define ADC_ENABLE()        LPC_ADC->ADCR |= ADCR_PDN
-#define ADC_DISABLE()       LPC_ADC->ADCR &= ~ADCR_PDN
-#define ADC_SET_BURSTMODE() LPC_ADC->ADCR |= ADCR_BURST 
-
-#define PWM_ENABLE_IRQ()            NVIC_EnableIRQ(PWM1_IRQn)
-
-#define ADC_DISABLE_IRQ()           NVIC_DisableIRQ(ADC_IRQn)
-#define ADC_SET_ALL_IRQS_BURST()    LPC_ADC->ADINTEN = 0xFF
-#define ADC_SET_ALL_IRQS()          LPC_ADC->ADINTEN = 0x1FF
-
-#define ADC_START()         LPC_ADC->ADCR |= (1 << 24)
-#define ADC_STOP()          LPC_ADC->ADCR &= ~(0x7 << 24)
-
-// Working with the ADC output register
-#define ADC_HAS_OVERRUN(x)  x & ADC_OVERRUN
-#define ADC_GET_VALUE(x)    (x >> 4) & 0xFFFF;
+#define PWM_ENABLE_PIN0()   LPC_PINCON->PINSEL4  |= (0b01 << 0); LPC_PINCON->PINMODE4 |= (0b00 << 0)  // PWM 1.1 func 01 with pull-up Resistor
+#define PWM_ENABLE_PIN1()   LPC_PINCON->PINSEL4  |= (0b01 << 2); LPC_PINCON->PINMODE4 |= (0b00 << 2)
+#define PWM_ENABLE_PIN2()   LPC_PINCON->PINSEL4  |= (0b01 << 4); LPC_PINCON->PINMODE4 |= (0b00 << 4)
+#define PWM_ENABLE_PIN3()   LPC_PINCON->PINSEL4  |= (0b01 << 6); LPC_PINCON->PINMODE4 |= (0b00 << 6)
+#define PWM_ENABLE_PIN4()   LPC_PINCON->PINSEL4  |= (0b01 << 8); LPC_PINCON->PINMODE4 |= (0b00 << 8)
+#define PWM_ENABLE_PIN5()   LPC_PINCON->PINSEL4  |= (0b01 << 10); LPC_PINCON->PINMODE4 |= (0b00 << 10)
 
 int8 initializePWM(uint32 freq,float duty, uint8 pin);
 inline void startPWM(uint8 pin);
